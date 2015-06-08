e6216f43f48006b15e55ca6d9798c993707ba20f
Ralf Jung <post@ralfj.de>
2015-06-08 15:28:06 +0200

    link to some installation instructions

--- src/main.rs
+++ src/main.rs
@@ -9,13 +9,27 @@
 // what to explain in which order, and making sure I have sample code for all topics
 // I plan to cover. They may also be helpful as an offline resource, but you're on your
 // own then.
-
+// 
 // I will assume basic familiarity with programming, and hence not explain the basic
 // concepts common to most languages. Instead, I will focus on what makes Rust special.
-
+//
+// Prerequisites
+// -------------
+//
+// You will need to have Rust installed, of course. It is available for download on
+// [the Rust website](http://www.rust-lang.org/). At this point, I plan to restrict
+// myself to stable Rust, so "Recommended" version is just right.
+// You can find some more installation instructions in
+// [the second chapter of The Book](https://doc.rust-lang.org/stable/book/getting-started.html).
+// When you are done, running `cargo build` in the root of Rust-101 should successfully compile
+// all the code.
+//
+// Course Content
+// --------------
+// 
 // The actual course is in the partXX.rs files. I suggest you get started with
 // [the first part](part00.html), or jump directly to where you left off:
-
+// 
 // * [Part 00](part00.html)
 // * [Part 01](part01.html)
 mod part00;
@@ -33,6 +47,7 @@ fn main() {
 // 
 // There's tons of useful Rust stuff out there, so let me just put links to some
 // of the most interesting places here:
+// 
 // * [The Rust Book](https://doc.rust-lang.org/stable/book/)
 // * [Rust by Example](http://rustbyexample.com/)
 // * The [Rust Subreddit](https://www.reddit.com/r/rust/)
