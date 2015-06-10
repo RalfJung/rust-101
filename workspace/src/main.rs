943c00ca03ddc76177b4a16e19e8b831247e03f8
Ralf Jung <post@ralfj.de>
2015-06-10 16:20:20 +0200

    tweak workspace

--- workspace/src/main.rs
+++ workspace/src/main.rs
@@ -2,6 +2,8 @@ mod part00;
 mod part01;
 mod part02;
 
+#[cfg(not(test))] /* If you get warnings about functions not being used on "crate test", adding this attribute will fix them.
+                     It says that the function is only to be compiled if we are *not* compiling for tests. */
 fn main() {
    part00::part_main();
 }
