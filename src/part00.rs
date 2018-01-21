e6c369cfcd2d8ae38eb9c5cfbbee685bae2fe64d
Nicola 'tekNico' Larosa <nico@teknico.net>
2018-01-21 19:21:55 +0100

    part00.rs lines shortened

--- src/part00.rs
+++ src/part00.rs
@@ -5,11 +5,12 @@
 // minimum of a list.
 
 //@ ## Getting started
+
 //@ Let us start by thinking about the *type* of our function. Rust forces us to give the types of
-//@ all arguments, and the return type, before we even start writing the body. In the case of our minimum
-//@ function, we may be inclined to say that it returns a number. But then we would be in trouble: What's
-//@ the minimum of an empty list? The type of the function says we have to return *something*.
-//@ We could just choose 0, but that would be kind of arbitrary. What we need
+//@ all arguments, and the return type, before we even start writing the body. In the case of our
+//@ minimum function, we may be inclined to say that it returns a number. But then we would be in
+//@ trouble: What's the minimum of an empty list? The type of the function says we have to return
+//@ *something*. We could just choose 0, but that would be kind of arbitrary. What we need
 //@ is a type that is "a number, or nothing". Such a type (of multiple exclusive options)
 //@ is called an "algebraic datatype", and Rust lets us define such types with the keyword `enum`.
 //@ Coming from C(++), you can think of such a type as a `union`, together with a field that
@@ -37,22 +38,24 @@ fn vec_min(vec: Vec<i32>) -> NumberOrNothing {
 
     // Now we want to *iterate* over the list. Rust has some nice syntax for iterators:
     for el in vec {
-        // So `el` is an element of the list. We need to update `min` accordingly, but how do we get the current
-        // number in there? This is what pattern matching can do:
+        // So `el` is an element of the list. We need to update `min` accordingly, but how do we
+        // get the current number in there? This is what pattern matching can do:
         match min {
-            // In this case (*arm*) of the `match`, `min` is currently nothing, so let's just make it the number `el`.
+            // In this case (*arm*) of the `match`, `min` is currently nothing, so let's just make
+            // it the number `el`.
             NumberOrNothing::Nothing => {
                 min = NumberOrNothing::Number(el);                  /*@*/
             },
-            // In this arm, `min` is currently the number `n`, so let's compute the new minimum and store it.
+            // In this arm, `min` is currently the number `n`, so let's compute the new minimum and
+            // store it.
             //@ We will write the function `min_i32` just after we completed this one.
             NumberOrNothing::Number(n) => {
                 let new_min = min_i32(n, el);                       /*@*/
                 min = NumberOrNothing::Number(new_min);             /*@*/
             }
         }
-        //@ Notice that Rust makes sure you did not forget to handle any case in your `match`. We say
-        //@ that the pattern matching has to be *exhaustive*.
+        //@ Notice that Rust makes sure you did not forget to handle any case in your `match`. We
+        //@ say that the pattern matching has to be *exhaustive*.
     }
     // Finally, we return the result of the computation.
     return min;
